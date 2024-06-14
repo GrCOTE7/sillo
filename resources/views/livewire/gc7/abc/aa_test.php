d2e9a3326ca4df01368a48663538a0cee7b549ef
GrCOTE7 <grcote7@gmail.com>
2024-06-14 21:47:09 +0200

    Feat: Simple partial

---
+++ resources/views/livewire/gc7/abc/aa_test.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * (ɔ) LARAVEL.Sillo.org - 2015-2024
+ */
+
+use Livewire\Attributes\Layout;
+use Livewire\Volt\Component;
+
+new 
+#[Layout('components.layouts.auth')]
+class extends Component {
+	public $name;
+	public function mount()
+	{
+		// $this->name = 'abc';
+	}
+
+	public function updatedName()
+	{
+		$this->name = strtoupper($this->name);
+	}
+
+	public function with()
+	{
+		return [
+			// 'name' => $this->name,
+		];
+	}
+};

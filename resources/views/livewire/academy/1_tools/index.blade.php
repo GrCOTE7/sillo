4a39ff146d8a0c04ae98746d6fb238b1688e6f2f
GrCOTE7 <grcote7@gmail.com>
2024-08-09 17:19:07 +0200

    Feat: Inster .md in blade view

--- resources/views/livewire/academy/1_tools/index.blade.php
+++ resources/views/livewire/academy/1_tools/index.blade.php
@@ -3,15 +3,17 @@
 use Livewire\Attributes\{Layout, Title};
 use Livewire\Volt\Component;
 
-new
-#[Title('Tools')]
-#[Layout('components.layouts.academy')]
-class extends Component {
+new #[Title('Tools')] #[Layout('components.layouts.academy')] class extends Component {
     //
 }; ?>
 
 <div>
     <x-header class="mb-0" title="Outils préconisés" shadow separator progress-indicator />
-    
+
     //2do Présentation des outils présentés ici + liens / page des détails
+
+    <x-markdown>
+        @include('livewire.academy.1_tools/README.md')
+    </x-markdown>
+
 </div>

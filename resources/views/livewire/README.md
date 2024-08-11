341af80b65054d2bf61fe3dd976df798b89f7bb6
GrCOTE7 <grcote7@gmail.com>
2024-08-11 16:16:43 +0200

    Feat: Nett

---
+++ resources/views/livewire/README.md
@@ -0,0 +1,10 @@
+# Outils préconisés
+
+- Git
+
+- Éditeur (VSC-Extensions majeures - Todo, GitGraph, Codium, markdown preview)
+
+- Serveurs (PHP, MySQL, Mail)
+
+---
+
--- resources/views/livewire/academy/1_tools/index.blade.php
+++ resources/views/livewire/academy/1_tools/index.blade.php
@@ -3,17 +3,19 @@
 use Livewire\Attributes\{Layout, Title};
 use Livewire\Volt\Component;
 
-new #[Title('Tools')] #[Layout('components.layouts.academy')] class extends Component {
+new #[Title('Tools')] #[Layout('components.layouts.academy')] 
+class extends Component {
     //
 }; ?>
 
 <div>
+    @php
+        $content = file_get_contents(resource_path('views/livewire/academy/1_tools/README.md'));
+    @endphp
     <x-header class="mb-0" title="Outils préconisés" shadow separator progress-indicator />
 
     //2do Présentation des outils présentés ici + liens / page des détails
-
-    <x-markdown>
-        @include('livewire.academy.1_tools/README.md')
-    </x-markdown>
-
+    
+    {!! $content !!}
+    
 </div>
